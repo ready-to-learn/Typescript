a89124a83072017d13bb4b4ebbac98c6d5c64aa9
Jayalakshmi <jaiindira@gmail.com>
2018-11-07 00:02:05 +0530

    Code changes in Home screen

--- TypescriptDemo/src/home/home.component.ts
+++ TypescriptDemo/src/home/home.component.ts
@@ -1,26 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
+enum mobPrice{Black=25000,White=28000,Gold=30000};
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
-   discount : number;
-   totalAmt : number;
-
-
-  constructor() { }
+  discount : number;
+  totalAmt : number;
+  mobiles : any;
+  constructor() { 
+  }
 
   ngOnInit() {
  console.log("Mobile Price - " + this.calculatePrice(28000));
-    
+ this.mobiles= [
+  {
+    name: "Sony Xperia Z2",
+    image_path: "assets/Sony_Xperia_Z2.png",
+    price: mobPrice.Black,
+  },
+  {
+    name: "Sony Xperia R1",
+    image_path: "assets/Sony_Xperia_R1.png" ,
+    price: mobPrice.White,
+  },
+  {
+    name: "Sony Xperia R1 Plus",
+    image_path: "assets/Sony_Xperia_R1_Plus.png",
+    price: mobPrice.Gold
+  }
+
+];
+    console.log(this.mobiles[0].image_path);
   }
 
  calculatePrice(price:number):number{
  
-
-  enum mobPrice{Black=25000,White=28000,Gold=30000};
 if(mobPrice.Black==price)
 {
   this.discount=5;
